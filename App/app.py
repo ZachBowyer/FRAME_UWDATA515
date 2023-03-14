e2248ca01ea339b660cf564aabb0e061f049ca33
Arjun Sharma <65405284+Arjun-SC31@users.noreply.github.com>
2023-03-13 22:53:41 -0700

    updated app.py to retain singleton comparison

--- App/app.py
+++ App/app.py
@@ -119,7 +119,7 @@ def restaurant_category_shortlist(filter_score, restaurant_category_input):
             '|'.join(list_rest_category
                      )
             )
-    filter_rest_category = filter_score[filter_score['result'] is True]
+    filter_rest_category = filter_score[filter_score['result'] == True]# pylint: disable=singleton-comparison
     filter_rest_category.drop('result', axis = 1, inplace = True)
     #st.write(filter_rest_category.shape)
     return filter_rest_category
@@ -169,7 +169,7 @@ def health_inspect_shortlist(filter_food, health_inspect_input):
         st.error('Invalid Health Inspection result input, try again!', icon="🚨")
         st.stop()
     filter_food['result'] = filter_food['Grade'].str.contains('|'.join(acceptable))
-    health_inspection_df = filter_food[filter_food['result'] is True]
+    health_inspection_df = filter_food[filter_food['result'] == True]# pylint: disable=singleton-comparison
     health_inspection_df.drop('result', axis = 1, inplace = True)
     return health_inspection_df
 
