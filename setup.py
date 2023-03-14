93e2f453a06352c4b4a58517b31abfe482952a00
Zachary Bowyer <zwbowyer@gmail.com>
2023-03-13 17:25:17 -0700

    Added preprocess to google drive

--- setup.py
+++ setup.py
@@ -21,10 +21,18 @@ zipcode = search.by_zipcode(99338)
 URL1 = 'https://drive.google.com/uc?id=19x38WTsXuYc7hV7CWHJgal9KhuAVyjxP'
 gdown.download(URL1, 'data/restaurant-menus.csv', quiet=False)
 
-# Retaurant Data
+# Restaurant Data
 URL2 = 'https://drive.google.com/uc?id=11jI-5gKmaA_kx3Owv757aqXbSIqkzKWn'
 gdown.download(URL2, 'data/restaurants.csv', quiet=False)
 
 # King county inspection data
 URL3 = 'https://drive.google.com/uc?id=1EPtMo7mulWnW2sRrZoHHWFfnU54pYmCX'
 gdown.download(URL3, 'data/seagov.csv', quiet=False)
+
+# Preprocessed data (Takes too long to run manually)
+URL4 = 'https://drive.google.com/file/d/149o_vBeYkXa0oAGPUNJ3FUeWdFicO-0e/view?usp=share_link'
+gdown.download(URL4, 'data/Category_mapping.csv', quiet=False)
+
+# Preprocessed data (Takes too long to run manually)
+URL5 = 'https://drive.google.com/file/d/10g9vS2VFuGYmxLBCRnxjSvXVLlrpEnKv/view?usp=share_link'
+gdown.download(URL5, 'data/Food_and_Restaurant_Data.csv', quiet=False)
